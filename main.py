dd722d770b82f42a587f6e6beca71df1c2fbcb23
amarkules1 <amarlar2354@gmail.com>
2023-02-04 00:27:22 -0500

    create working service

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
+import json
+
 from flask import Flask, jsonify, make_response
 import requests
 import os
+import pandas as pd
+import sqlalchemy
+from sqlalchemy import sql
 
 
 app = Flask(__name__)
 
 
-@app.route("/", methods=['GET'])
-def hello():
-    ''' Greet the user '''
+secret_sauce = json.load(open('secret_sauce.json',))
+conn = sqlalchemy.create_engine(secret_sauce['db_conn_string']).connect()
 
-    return "Hey! The service is up, how about doing something useful"
+
+@app.route("/projects", methods=['GET'])
+def projects():
+    data = pd.read_sql(sql.text("select * from portfolio_projects"), conn)
+    data = data.sort_values(['priority', 'created_at'], ascending=[True, False])[['id', 'url', 'title', 'description']]
+    return data.to_json(orient="records")
 
 
 if __name__ == '__main__':
